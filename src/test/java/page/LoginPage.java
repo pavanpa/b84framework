974b9e60a1c202cdfd5f1004d51e7b9e39d4db63
user <user@DESKTOP-VKSQJFC>
2023-12-29 21:18:15 +0530

    LoginPageUpdated

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class loginPage 
+public class LoginPage 
 {
 	@FindBy(name="username")
 	private WebElement unTB;
